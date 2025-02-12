3d0b52c3436c534efce6218d9333377dbb7d2eaf
Dmitrii Danilov <85995347+danilovdima5@users.noreply.github.com>
2025-02-13 02:31:06 +0500

    init firebase app

---
+++ firebase.options.ts
@@ -0,0 +1,24 @@
+import { FirebaseOptions } from '@angular/fire/app';
+
+const projectId = 'my-map-2025-angular';
+
+const appId = '1:978964810192:web:c21e604971166e24bb91d5';
+
+const storageBucket = 'my-map-2025-angular.firebasestorage.app';
+
+const apiKey = 'AIzaSyCEbfp8b-gKes3ifhBr-Iaav15KdsUwhXE';
+
+const authDomain = 'my-map-2025-angular.firebaseapp.com';
+
+const messagingSenderId = '978964810192';
+
+const firebaseOptions: FirebaseOptions = {
+  projectId,
+  appId,
+  storageBucket,
+  apiKey,
+  authDomain,
+  messagingSenderId,
+};
+
+export default firebaseOptions;
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,18 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { routes } from './app.routes';
+
+import firebaseOptions from '../../firebase.options';
+
+const firebaseApp = initializeApp(firebaseOptions);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimations(),
+    provideFirebaseApp(() => firebaseApp),
   ],
 };
